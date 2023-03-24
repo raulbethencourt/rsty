0dc26a9f9a107bfaee5f22275cdd308bc7a3e8ee
Raul Bethencourt <raul.bethencourt.pro@gmail.com>
2023-03-24 22:33:19 +0100

    :construction: create frontend controller

--- frontend/src/controllers.rs
+++ frontend/src/controllers.rs
@@ -1,1 +1,53 @@
-// TODO: continue 4:40
+use crate::{
+    state::{TaskAction, TaskState},
+    todo_api,
+};
+use yew::UseReducerHandle;
+
+pub struct TaskController {
+    state: UseReducerHandle<TaskState>,
+}
+
+impl TaskController {
+    pub fn new(state: UseReducerHandle<TaskState>) -> TaskController {
+        TaskController { state }
+    }
+
+    pub fn init_tasks(&self) {
+        let tasks = self.state.clone();
+        wasm_bindgen_futures::spawn_local(async move {
+            let fetched_tasks = todo_api::fetched_tasks().await.unwrap();
+            tasks.dispatch(TaskAction::Set(fetched_tasks))
+        });
+    }
+    
+    pub fn create_task(&self, title: String) {
+        let tasks = self.state.clone();
+        wasm_bindgen_futures::spawn_local(async move {
+            let response = todo_api::create_task().await.unwrap();
+            tasks.dispatch(TaskAction::Add(response))
+        });
+    }
+    
+    pub fn toggle_task(&self, id: String) {
+        let tasks = self.state.clone();
+        wasm_bindgen_futures::spawn_local(async move {
+            let response = todo_api::toggle_task().await.unwrap();
+            if response.rows_affected == 1 {
+                tasks.dispatch(TaskAction::Toggle(id.clone()));
+            }
+        });
+    }
+    
+    pub fn delete_task(&self, id: String) {
+        let tasks = self.state.clone();
+        wasm_bindgen_futures::spawn_local(async move {
+            let response = todo_api::delete_task().await.unwrap();
+            if response.rows_affected == 1 {
+                tasks.dispatch(TaskAction::Delete(id.clone()));
+            }
+        });
+    }
+}
+
+
